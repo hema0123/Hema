38c9e3edcbee8f96676d291dd4bee16a74d9e713
hema <jhema0123@gmail.com>
2018-10-24 10:13:48 +0530

    delete one file

---
+++ practise/script_file1.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+#example for if else command
+#created by hema on 23 oct
+x=8
+if [ $x -gt 10 ]; then
+	echo "$x value is more than 10"
+else 
+	echo "$x value is less than 10"
+fi
---
+++ practise/script_file2.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+#example of using arguements
+#created by hema on 23 oct
+echo "my first name $1"
+echo "my last name $2"
+echo "my number is $3"
